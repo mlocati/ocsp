b99917c198a38153b6671fccc6e4f03152bd3ce5
bseddon <bill.seddon@lyquidity.com>
2021-06-04 21:00:20 +0000

    Changes location

---
+++ src/autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+function ocsp_requestor_autoload( $classname )
+{
+	$prefix = 'Ocsp\\';
+	if ( strpos( $classname, $prefix ) !== 0 ) return false;
+	$filename = substr( $classname, strlen( $prefix ) ) . '.php';
+	// $filename = "$classname.php";
+	require_once  __DIR__ . "/$filename";
+}
+
+spl_autoload_register( 'ocsp_requestor_autoload' );
